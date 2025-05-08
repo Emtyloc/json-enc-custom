04bfbb7dd653df7898e97092ea700b006d164577
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2025-05-07 19:03:05 -0700

    Add support for multipart/form-data
    - The way it works is that if 'hx-multipart' attribute is passed, then instead of encoding parameters in 'text/json' it get's encoded into 'multipart/form-data'.
    - This enables to support for uploading file(s) as it requires the form to be 'multipart'.
    - Despite the mime-type is multipart, the parameters that are not an 'instanceof File' are encoded as if it's a normal json data.
    - The only quirk of this implementation is that multipart data will always be encoded in the following format: `{ 'data': <parameters that are not File>, 'file': <file data>  }.
    - PS: My editor has auto-format with 'Prettier' setup and hence one might see some formatting changes like changing from `'` -> `"`, so sorry in advance.

--- json-enc-custom.js
+++ json-enc-custom.js
@@ -1,24 +1,32 @@
 (function () {
     let api;
-    htmx.defineExtension('json-enc-custom', {
+    htmx.defineExtension("json-enc-custom", {
         init: function (apiRef) {
-            api = apiRef
+            api = apiRef;
         },
         onEvent: function (name, evt) {
             if (name === "htmx:configRequest") {
-                evt.detail.headers['Content-Type'] = "application/json";
+                let element = evt.detail.elt;
+                if (element.hasAttribute("hx-multipart")) {
+                    evt.detail.headers["Content-Type"] = "multipart/form-data";
+                } else {
+                    evt.detail.headers["Content-Type"] = "application/json";
+                }
             }
         },
         encodeParameters: function (xhr, parameters, elt) {
-            xhr.overrideMimeType('text/json');
+            if (!elt.hasAttribute("hx-multipart")) {
+                xhr.overrideMimeType("text/json");
+            }
 
             let encoded_parameters = encodingAlgorithm(parameters, elt);
 
             return encoded_parameters;
-        }
+        },
     });
 
     function encodingAlgorithm(parameters, elt, includedElt) {
+        let files = [];
         let resultingObject = Object.create(null);
         const PARAM_NAMES = Object.keys(parameters);
         const PARAM_VALUES = Object.values(parameters);
@@ -28,34 +36,52 @@
             let name = PARAM_NAMES[param_index];
             let value = PARAM_VALUES[param_index];
 
-            const elements = getChildrenByName(elt, name);
-            if (isSelectMultiple(elements) && !Array.isArray(value)) {
-                value = [value]; // force the value of select multiple to be an array
-            }
+            if (value instanceof File) {
+                files.push(value);
+            } else if (
+                Array.isArray(value) &&
+                Object.values(value).every((val) => val instanceof File)
+            ) {
+                files = Object.values(value);
+            } else {
+                const elements = getChildrenByName(elt, name);
+                if (isSelectMultiple(elements) && !Array.isArray(value)) {
+                    value = [value]; // force the value of select multiple to be an array
+                }
 
-            let parse_value = api.getAttributeValue(elt, "parse-types");
-            if (parse_value === "true" ) {
-                let includedElt = getIncludedElement(elt);
-                value = parseValues(elements, includedElt, value);
-            }
+                let parse_value = api.getAttributeValue(elt, "parse-types");
+                if (parse_value === "true") {
+                    let includedElt = getIncludedElement(elt);
+                    value = parseValues(elements, includedElt, value);
+                }
 
-            let steps = JSONEncodingPath(name);
-            let context = resultingObject;
+                let steps = JSONEncodingPath(name);
+                let context = resultingObject;
 
-            for (let step_index = 0; step_index < steps.length; step_index++) {
-                let step = steps[step_index];
-                context = setValueFromPath(context, step, value);
+                for (let step_index = 0; step_index < steps.length; step_index++) {
+                    let step = steps[step_index];
+                    context = setValueFromPath(context, step, value);
+                }
             }
         }
 
         let result = JSON.stringify(resultingObject);
-        return result
+        if (elt.hasAttribute("hx-multipart")) {
+            const formData = new FormData();
+            formData.append("data", result);
+            for (const file of files) {
+                formData.append("file", file);
+            }
+            return formData;
+        } else {
+            return result;
+        }
     }
 
     function getChildrenByName(original, name) {
         const match = `[name="${name}"]`;
         // find the closest owning form and use this as the root element for finding matches
-        return original.closest('form').querySelectorAll(match);
+        return original.closest("form").querySelectorAll(match);
     }
 
     function isSelectMultiple(elements) {
@@ -63,26 +89,26 @@
             elements.length === 1 &&
             elements[0] instanceof HTMLSelectElement &&
             elements[0].type === "select-multiple"
-        )   
+        );
     }
 
     function parseValues(elements, includedElt, value) {
         if (!elements.length && includedElt !== undefined) {
             // "hx-include" allows CSS query selectors which may return an specific node, e.g a single input
             if (includedElt.matches(match)) {
-                elements = [includedElt]
+                elements = [includedElt];
             } else {
                 elements = includedElt.querySelectorAll(match);
             }
         }
 
         if (!Array.isArray(value)) return parseElementValue(elements[0], value);
-        
+
         if (isSelectMultiple(elements)) {
             const elt = elements[0];
             const convertToNumber = checkAllPossibleOptionsAreNumbers(elt);
             for (let index = 0; index < value.length; index++) {
-                let arrayValue = value[index]
+                let arrayValue = value[index];
                 if (convertToNumber) {
                     arrayValue = Number(arrayValue);
                 }
@@ -101,26 +127,29 @@
 
     function parseElementValue(elt, value) {
         switch (true) {
-        case elt instanceof HTMLInputElement:
-            switch (elt.type) {
-            case "checkbox":
-                return elt.checked;
-            case "number":
-            case "range": 
-                return Number(value);
-            }
-            break;
-        case elt instanceof HTMLSelectElement:
-            if (elt.type === "select-one" && checkAllPossibleOptionsAreNumbers(elt)) {
-                return Number(value);
-            }
-            break;
-        }        
+            case elt instanceof HTMLInputElement:
+                switch (elt.type) {
+                    case "checkbox":
+                        return elt.checked;
+                    case "number":
+                    case "range":
+                        return Number(value);
+                }
+                break;
+            case elt instanceof HTMLSelectElement:
+                if (
+                    elt.type === "select-one" &&
+                    checkAllPossibleOptionsAreNumbers(elt)
+                ) {
+                    return Number(value);
+                }
+                break;
+        }
         return value;
     }
 
     function checkAllPossibleOptionsAreNumbers(elt) {
-        const values = [...elt.options].map(o => o.value);
+        const values = [...elt.options].map((o) => o.value);
         if (values.length == 0) {
             return true;
         }
@@ -135,7 +164,9 @@
     function JSONEncodingPath(name) {
         let path = name;
         let original = path;
-        const FAILURE = [{ "type": "object", "key": original, "last": true, "next_type": null }];
+        const FAILURE = [
+            { type: "object", key: original, last: true, next_type: null },
+        ];
         let steps = Array();
         let first_key = String();
         for (let i = 0; i < path.length; i++) {
@@ -144,23 +175,33 @@
         }
         if (first_key === "") return FAILURE;
         path = path.slice(first_key.length);
-        steps.push({ "type": "object", "key": first_key, "last": false, "next_type": null });
+        steps.push({
+            type: "object",
+            key: first_key,
+            last: false,
+            next_type: null,
+        });
         while (path.length) {
             // []
             if (path.startsWith("[]")) {
                 path = path.slice(2);
-                steps.push({ "type": "array", "key": 0, "last": false, "next_type": null })
+                steps.push({ type: "array", key: 0, last: false, next_type: null });
                 continue;
             }
             // [123...]
             if (/^\[\d+\]/.test(path)) {
                 path = path.slice(1);
-                let collected_digits = path.match(/\d+/)[0]
+                let collected_digits = path.match(/\d+/)[0];
                 path = path.slice(collected_digits.length);
                 let numeric_key = parseInt(collected_digits, 10);
                 path = path.slice(1);
-                steps.push({ "type": "array", "key": numeric_key, "last": false, "next_type": null });
-                continue
+                steps.push({
+                    type: "array",
+                    key: numeric_key,
+                    last: false,
+                    next_type: null,
+                });
+                continue;
             }
             // [abc...]
             if (/^\[[^\]]+\]/.test(path)) {
@@ -169,7 +210,12 @@
                 path = path.slice(collected_characters.length);
                 let object_key = collected_characters;
                 path = path.slice(1);
-                steps.push({ "type": "object", "key": object_key, "last": false, "next_type": null });
+                steps.push({
+                    type: "object",
+                    key: object_key,
+                    last: false,
+                    next_type: null,
+                });
                 continue;
             }
             return FAILURE;
@@ -179,8 +225,7 @@
                 let tmp_step = steps[step_index];
                 tmp_step["last"] = true;
                 steps[step_index] = tmp_step;
-            }
-            else {
+            } else {
                 let tmp_step = steps[step_index];
                 tmp_step["next_type"] = steps[step_index + 1]["type"];
                 steps[step_index] = tmp_step;
@@ -194,7 +239,7 @@
             context[step.key] = value;
         }
 
-        //TODO: make merge functionality and file suport.
+        //TODO: make merge functionality.
 
         //check if the context value already exists
         if (context[step.key] === undefined) {
@@ -218,8 +263,7 @@
                     return context[step.key];
                 }
             }
-        }
-        else {
+        } else {
             return context[step.key];
         }
     }
@@ -229,11 +273,11 @@
 
         if (includedSelector) {
             // "hx-include" can be inherited so `elt` will not always be the root element
-            let eltWithInclude = api.getClosestMatch(elt, function(e) {
-              return e.matches(`[hx-include="${includedSelector}"]`);
-            })
+            let eltWithInclude = api.getClosestMatch(elt, function (e) {
+                return e.matches(`[hx-include="${includedSelector}"]`);
+            });
 
-            return api.querySelectorExt(eltWithInclude, includedSelector)
+            return api.querySelectorExt(eltWithInclude, includedSelector);
         }
     }
-})()
+})();
